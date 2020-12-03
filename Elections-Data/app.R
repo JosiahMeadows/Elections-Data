f24a07fa0bf464a84f896db09edc8edff60ecd8b
Marlena Meadows <marlenameadows@Josiahs-MacBook-Pro-2.local>
2020-12-03 03:42:06 -0500

    Completed maps. Model remains

--- Elections-Data/app.R
+++ Elections-Data/app.R
@@ -20,55 +20,49 @@ combined_fl <- readRDS("combined_fl.rds")
 combined_2 <- readRDS("combined_2.rds")
 covid <- readRDS("covid.RDS")
 combined_intermediate <- readRDS("combined_intermediate.RDS")
+combined_full <- readRDS("combined_full.RDS")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(theme = shinytheme("lumen"),
     navbarPage(
         "Voter Registrations",
         
-        tabPanel("About",
-                 # Application title
-                 titlePanel("Driving Forces behind Voter Registrations"),
-                 br(),
-                 h3("About this Project"),
-                 p("I spent a while looking through a spreadsheet of datasets my TF shared with me, and I found a category on voter registration. Ever since starting Gov 50, I knew I wanted to do something with elections, so this category made sense. I wanted to display data registration levels by month, year, and by party affiliation. I thought it would be interesting to hone in on a swing state like Florida and see whether more Democrats are registering than Republicans or vice versa. I also thought it would be interesting to create vertical lines to denote significant events which may have caused an increase in voter registration (e.g. the killing of George Floyd). The Florida Department of State Website offers datasets with this information. I created a visualization in Shiny App. I need to figure out how to rearrange the months in the x-axis. Instead of displaying all of the states in one graph, I intend to create an input section where the user can choose a state and see the data."),
-                 br(),
-                 h3("About Me"),
-                 br(),
-                 h4("Josiah Meadows"),
-                 p("I am a sophomore at Harvard University pursuing a B.A. in Government with a secondary in Economics."),
-                 p("The source code on Github can be found",
-                   a("here.",
-                     href = "https://github.com/JosiahMeadows/Elections-Data"))
-                 
-                 # Sidebar with a slider input for number of bins
-                 
-                 
-        ),
-        
         tabPanel("Data Visualization",
                  # Application title
-                 titlePanel("Voter Registration"),
+                 titlePanel("The Driving Forces Behind Voter Registrations"),
                  
                  br(),
-                 h2("About the Data"),
+                 h2("Graphs"),
+                 br(),
+                 p("First, I created two regressions with vertical lines 
+                   denoting the occurrence of significant events in 2020. The
+                   goal was to see if there was any change immediately before or
+                   after the vertical lines. The first graphs breaks down the 
+                   Florida monthly registration numbers by party, and the second
+                   adds all of the parties into one line."),
                  
                  br(),
-                 p("According to the regression, there does not seem to be any
-                   significant change immediately after the events indicated
-                   by the vertical lines. It does appear, as one might expect,
-                   that registrations began to slow as the pandemic began to 
-                   accelerate. Voter registrations did increase after the 
-                   killing of George Floyd, but they were began increasing 
-                   in May before his death."),
+                 p("According to both of the regressions below, there does not
+                 seem to be any significant change in voter registration levels
+                 immediately after the events indicated by the vertical lines.
+                 It does appear, as one might expect, that registrations began
+                 to slow as the pandemic began to accelerate. Voter
+                 registrations did increase after the killing of George Floyd,
+                 but they were already increasing in May before his death."),
                  
                  br(),
                  plotOutput("registration_numbers"),
                  plotOutput("registration_numbers2"),
                  
-
+                 h2("Maps"),
+                 p("Second, I create a couple of colorful interactive maps to 
+                   see if there might be any clear visual patterns between the 
+                   datasets."),
+                 br(),
                  h3("Total Voter Registrations by Month in Florida"),
-                 h4("(Hover over the map to see the county name and voter registration numbers.)"),
+                 h4("(Hover over the map to see the county name and voter
+                    registration numbers.)"),
+                 
                  br(),
                  selectInput("A", "Choose month:",
                              c("January" = "jan",
@@ -79,17 +73,77 @@ ui <- fluidPage(theme = shinytheme("lumen"),
                                "June" = "jun",
                                "July" = "jul",
                                "August" = "aug")),
-                 selectInput("A2", "Choose party:", combined_intermediate$party, selected = "florida_democratic_party"),
+                 
+                 selectInput("A2", "Choose party:",
+                             c("Democrat" = "florida_democratic_party",
+                               "Republican" = "republican_party_of_florida",
+                               "Minor Party" = "minor_party",
+                               "No Party Affiliation" = "no_party_affiliation",
+                               "Totals" = "totals")),
+      
+                 #selectInput("A2", "Choose party:", combined_full$party,
+                             #selected = "florida_democratic_party"),
+                 
                  leafletOutput("registration_numbers3"),
+                 
+                 br(),
                  h3("New COVID-19 Cases in Florida by Month"),
-                 h4("(Hover over the map to see the county name and number of Covid cases.)"),
+                 h4("(Hover over the map to see the county name and number of 
+                    cases and deaths.)"),
+                 
                  br(),
                  selectInput("B", "Choose month:",
-                             c("January" = 1, "February" = 2, "March" = 3, "April" = 4, "May" = 5,
-                               "June" = 6, "July" = 7, "August" = 8, "September" = 9,
-                               "October" = 10, "November" = 11, "December" = 12)),
-                 leafletOutput("covid_cases"),
+                             c("January" = 1,
+                               "February" = 2,
+                               "March" = 3,
+                               "April" = 4,
+                               "May" = 5,
+                               "June" = 6,
+                               "July" = 7,
+                               "August" = 8,
+                               "September" = 9,
+                               "October" = 10,
+                               "November" = 11,
+                               "December" = 12)),
+                 leafletOutput("covid_cases")
+                 
+                 
+        ),
+        
+        tabPanel("Model"),
+        
+        tabPanel("About",
+                 br(),
+                 h3("About this Project"),
+                 p("The 2020 U.S. Presidential Election saw the highest voter
+                 turnout in 120 years. Voter enthusiasm was sky high across the 
+                 country. A devastating pandemic, economic pain, civil unrest,
+                 and movements for racial justice have often been cited as the
+                 leading factors which mobilized the masses. The goal of this 
+                 project is to determine how much of a correlation might exist
+                 between an increase in voter registration numbers and rising 
+                 COVID-19 cases and other significant events in 2020."),
+                 
+                 h3("About the Data"),
+                 p("I decided to hone in on the State of Florida, a critical 
+                   battleground state which many refer to a microcosm of U.S.
+                   politics. I gather the data on registration numbers from the
+                   official Florida department of state website and manipulated
+                   it for the purposes of this project. Next, I obtained data 
+                   on COVID-19 cases in Florida from the New York Times."),
+                 
+                 h3("About Me"),
+                 h4("Josiah Meadows"),
+                 p("I am a sophomore at Harvard University pursuing a B.A. in
+                   Government with a secondary in Economics."),
+                 p("The source code on
+                   Github can be found",
+                   a("here.",
+                     href = "https://github.com/JosiahMeadows/Elections-Data")),
+
         )
+        
+
         
     )
 )
@@ -183,20 +237,20 @@ server <- function(input, output) {
  
       county_shapes <- counties(state = "FL", cb = TRUE)
     
-      
-      sb_county <- combined_intermediate %>% 
+      sb_county <- combined_full %>% 
         filter(month == input$A) %>%
         filter(party == input$A2) %>% 
         clean_names() %>% 
         select(county, totals = value)
       
-      
       counties_merged_sb <- geo_join(county_shapes, sb_county, "NAME", "county")
       
-      pal <- colorBin("Greens", bins = c(10, 1000, 5000, 10000, 100000, 200000, Inf))
+      pal <- colorBin("Greens",
+                      bins = c(1000, 5000, 10000, 100000, 200000, Inf))
 
       popup_sb <- paste0("County: ", as.character(counties_merged_sb$NAME),
-                         "\nTotal Registrations: ", as.character(counties_merged_sb$totals))
+                         "\nTotal Registrations: ",
+                         as.character(counties_merged_sb$totals))
       
       leaflet() %>% 
         addProviderTiles("CartoDB.Positron") %>% 
@@ -213,7 +267,8 @@ server <- function(input, output) {
                       bringToFront = TRUE),
                     label = popup_sb,
                     labelOptions = labelOptions(
-                      style = list("font-weight" = "normal", padding = "3px 8px"),
+                      style = list("font-weight" = "normal",
+                                   padding = "3px 8px"),
                       textsize = "15px",
                       direction = "auto")) %>% 
         addLegend(pal = pal,
@@ -236,7 +291,10 @@ server <- function(input, output) {
       
       pal <- colorBin("Reds", bins = c(1000, 5000, 10000, 100000, 200000, Inf))
       
-      popup_sb2 <- paste0("New Cases: ", as.character(counties_merged_sb2$totals))
+      popup_sb2 <- paste0("New Cases: ",
+                          as.character(counties_merged_sb2$total_cases),
+                          "\nNew Deaths: ", 
+                          as.character(counties_merged_sb2$total_deaths))
       
       leaflet() %>% 
         addProviderTiles("CartoDB.Positron") %>% 
